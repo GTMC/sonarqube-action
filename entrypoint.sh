25ed0cea5f2f62443bb4efd33021e8f6952cbf1b
jtaschetti911 <44232750+jtaschetti911@users.noreply.github.com>
2020-08-27 15:53:48 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,4 @@ sonar-scanner \
 	-Dsonar.password=${INPUT_PASSWORD} \
 	-Dsonar.sources=. \
 	-Dsonar.sourceEncoding=UTF-8 \
-	-Dsonar.inclusions=file:${INPUT_INCLUSIONS}
+	-Dsonar.inclusions=${INPUT_INCLUSIONS}
