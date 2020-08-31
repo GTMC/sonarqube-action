bcc0e6f726f5cf857e90945d47195014b3183a4e
jtaschetti911 <44232750+jtaschetti911@users.noreply.github.com>
2020-08-31 13:47:32 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,3 @@ sonar-scanner \
 	-Dsonar.sources=. \
 	-Dsonar.sourceEncoding=UTF-8 \
 	-Dsonar.inclusions=${INPUT_INCLUSIONS}
-	-Dsonar.c.file.suffixes=-
-    	-Dsonar.cpp.file.suffixes=-
-    	-Dsonar.objc.file.suffixes=-
